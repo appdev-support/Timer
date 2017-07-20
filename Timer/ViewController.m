ccfef0af113d0bfe078512160649b64d599f07e8
Mrlijin <Mrlijin@users.noreply.github.com>
2017-07-20 20:24:12 +0800

    vds
    dfvdvd

--- Timer/ViewController.m
+++ Timer/ViewController.m
@@ -9,20 +9,50 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, readwrite, strong) NSTimer *timer;
+@property (nonatomic, readwrite, assign) NSInteger logTime;
 @end
-
+static NSTimeInterval sec = 1.0;
 @implementation ViewController
+- (IBAction)action:(id)sender {
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"" message:nil preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:(UIAlertActionStyleCancel) handler:^(UIAlertAction * _Nonnull action) {
+        [self invalidateTimer];
+    }];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"完成" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [alert addAction:cancelAction];
+    [alert addAction:okAction];
+    __weak typeof(self) weakSelf = self;
+    [self presentViewController:alert animated:YES completion:^{
+        [weakSelf timerBegin];
+    }];
+}
+
+- (void)timerBegin {
+    if (self.timer) {
+        [self invalidateTimer];
+    }
+    self.logTime = 3;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:sec repeats:YES block:^(NSTimer * _Nonnull timer) {
+        self.logTime--;
+        NSLog(@"%ld", (long)self.logTime);
+        if (!self.logTime) {
+            [self invalidateTimer];
+        }
+    }];
+    [self.timer fire];
+}
+
+- (void)invalidateTimer {
+    [self.timer invalidate];
+    self.timer = nil;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
